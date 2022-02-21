326723d643608d365e3c8d706eaaf8ac27ecaa6d
da-ladno-4e-tam <mixa.awesome@gmail.com>
2022-02-21 22:25:53 +0300

    Создал моки

---
+++ project/src/mocks/offers.ts
@@ -0,0 +1,168 @@
+import {Offer} from '../types/offer';
+
+export const offers: Offer[] = [
+  {
+    'bedrooms': 3,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10,
+      },
+      'name': 'Amsterdam',
+    },
+    'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'goods': [
+      'Heating',
+    ],
+    'host': {
+      'avatarUrl': 'img/3.png',
+      'id': 3,
+      'isPro': true,
+      'name': 'Angelina',
+    },
+    'id': 1,
+    'images': [
+      'img/1.png',
+    ],
+    'isFavorite': true,
+    'isPremium': false,
+    'location': {
+      'latitude': 52.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8,
+    },
+    'maxAdults': 4,
+    'previewImage': 'img/1.png',
+    'price': 120,
+    'rating': 4.8,
+    'title': 'Beautiful & luxurious studio at great location',
+    'type': 'apartment',
+  },
+  {
+    'bedrooms': 2,
+    'city': {
+      'location': {
+        'latitude': 43.370216,
+        'longitude': 7.895168,
+        'zoom': 10,
+      },
+      'name': 'Vena',
+    },
+    'description': 'Прикольное местечко.',
+    'goods': [
+      'Good 1',
+      'Good 2',
+    ],
+    'host': {
+      'avatarUrl': 'img/2.png',
+      'id': 2,
+      'isPro': true,
+      'name': 'Toma',
+    },
+    'id': 2,
+    'images': [
+      'img/1.png',
+      'img/2.png',
+    ],
+    'isFavorite': false,
+    'isPremium': true,
+    'location': {
+      'latitude': 42.35514938496378,
+      'longitude': 17.673877537499948,
+      'zoom': 8,
+    },
+    'maxAdults': 3,
+    'previewImage': 'img/2.png',
+    'price': 100,
+    'rating': 4.6,
+    'title': 'Norm',
+    'type': 'hotel',
+  },
+  {
+    'bedrooms': 1,
+    'city': {
+      'location': {
+        'latitude': 33.370216,
+        'longitude': 17.895168,
+        'zoom': 10,
+      },
+      'name': 'Paris',
+    },
+    'description': 'Ещё одно прикольное местечко.',
+    'goods': [
+      'Good 0',
+      'Good 1',
+      'Good 2',
+    ],
+    'host': {
+      'avatarUrl': 'img/1.png',
+      'id': 1,
+      'isPro': false,
+      'name': 'Amanda',
+    },
+    'id': 3,
+    'images': [
+      'img/0.png',
+      'img/1.png',
+      'img/2.png',
+    ],
+    'isFavorite': false,
+    'isPremium': false,
+    'location': {
+      'latitude': 32.35514938496378,
+      'longitude': 15.673877537499948,
+      'zoom': 8,
+    },
+    'maxAdults': 2,
+    'previewImage': 'img/3.png',
+    'price': 80,
+    'rating': 3.6,
+    'title': 'Norm place',
+    'type': 'room',
+  },
+  {
+    'bedrooms': 12,
+    'city': {
+      'location': {
+        'latitude': 23.370216,
+        'longitude': 27.895168,
+        'zoom': 10,
+      },
+      'name': 'Omsk',
+    },
+    'description': 'Офигенное место.',
+    'goods': [
+      'Good 0',
+      'Good 1',
+      'Good 2',
+      'Good 3',
+    ],
+    'host': {
+      'avatarUrl': 'img/4.png',
+      'id': 4,
+      'isPro': true,
+      'name': 'Andrey',
+    },
+    'id': 4,
+    'images': [
+      'img/0.png',
+      'img/1.png',
+      'img/2.png',
+      'img/3.png',
+    ],
+    'isFavorite': true,
+    'isPremium': true,
+    'location': {
+      'latitude': 22.35514938496378,
+      'longitude': 25.673877537499948,
+      'zoom': 8,
+    },
+    'maxAdults': 20,
+    'previewImage': 'img/4.png',
+    'price': 200,
+    'rating': 5,
+    'title': 'Great place',
+    'type': 'house',
+  },
+];
