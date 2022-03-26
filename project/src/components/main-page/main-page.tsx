5c8c9fc1830f214a5414cfdc036aa95e6e5bba2c
mikhail.biriukov <mikhail.biriukov@travelline.ru>
2022-03-26 15:58:12 +0300

    Подключил клмпонент main-empty.tsx

--- project/src/components/main-page/main-page.tsx
+++ project/src/components/main-page/main-page.tsx
@@ -8,6 +8,7 @@ import MainCityList from '../main-city-list/main-city-list';
 import MainSorting from '../main-sorting/main-sorting';
 import {useAppSelector} from '../../hooks';
 import {getSortedOffersList} from '../../const';
+import MainEmpty from '../main-empty/main-empty';
 
 function MainPage(): JSX.Element {
   const [activeOffer, setActiveOffer] = useState<Offer | null>(null);
@@ -29,27 +30,31 @@ function MainPage(): JSX.Element {
         <div className="tabs">
           <MainCityList/>
         </div>
-        <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{cityOffers.length} places to stay in {city.name}</b>
-              <MainSorting/>
-              <div className="cities__places-list places__list tabs__content">
-                <CardsList
-                  offers={getSortedOffersList(sortType, cityOffers)}
-                  onListItemMouseEnter={onListItemMouseEnter}
-                  onListItemMouseLeave={onListItemMouseLeave}
-                />
-              </div>
-            </section>
-            <div className="cities__right-section">
-              <section className="cities__map map">
-                <Map activeOffer={activeOffer} offers={cityOffers}/>
+        {cityOffers.length
+          ?
+          <div className="cities">
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{cityOffers.length} places to stay in {city.name}</b>
+                <MainSorting/>
+                <div className="cities__places-list places__list tabs__content">
+                  <CardsList
+                    offers={getSortedOffersList(sortType, cityOffers)}
+                    onListItemMouseEnter={onListItemMouseEnter}
+                    onListItemMouseLeave={onListItemMouseLeave}
+                  />
+                </div>
               </section>
+              <div className="cities__right-section">
+                <section className="cities__map map">
+                  <Map activeOffer={activeOffer} offers={cityOffers}/>
+                </section>
+              </div>
             </div>
           </div>
-        </div>
+          :
+          <MainEmpty/>};
       </main>
     </div>
   );
